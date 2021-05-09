d0984c109fbb02023f2d1a910062175560b9ca57
ulsyemr <ulsyemr@gmail.com>
2021-05-09 22:28:13 +0300

    port updated

--- app.js
+++ app.js
@@ -59,4 +59,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => console.log("listening on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("listening on port 3000")
+);
